ee723c2442fe69c524c2a9ff44e2d228dc46dc51
Abhinav <abhinavmule123@gmail.com>
2021-07-09 19:25:36 -0700

    Third Day Code And Task

---
+++ js/code.js
@@ -0,0 +1,68 @@
+// window.alert("this is alert");
+// document.write("hello world");
+// console.log("this");
+
+// // variable declartion ,  value assign 
+// var name; // declare 
+// name = "abhinav";  // value assign 
+// var name = "abhinav";
+// let name = "abhinav";
+// const name = "abhinav";
+
+// var num1 = 12;
+// let num2 = 13;
+// const pi = 3.14;
+
+// var and let -  you can change the value of var,let 
+// const -  never change 
+
+// var  name = "abhinav";
+// var name;
+// console.log(name);
+
+// let pi = 3.14;
+// pi = 2.12;
+// console.log(pi);
+
+/*
+let name = "abhinav"; //--  string 
+let  num1 = 123;         //-- number
+let num2 = "";
+let dataType = typeof name;
+let floatData = 12.212121233;
+document.write(floatData);
+*/
+
+//  declaring js variable 
+// let name = "abhinav"; 
+
+// single line  -- //
+// multiline line -- /* information */  /* */
+
+// let num1 = window.prompt("Enter your name");
+// // it returns null on cancel button click
+// window.alert(num1);
+
+// let returnValue = window.confirm("Are you sure , you want to log out !");
+
+// console.log(returnValue);
+
+
+// Arithmetic operator 
+let num1 = 20;
+let num2 = 30;
+document.write(num1+num2+"<br>");
+document.write(num1-num2+"<br>");
+document.write(num1*num2+"<br>");
+document.write(num1/num2+"<br>");
+document.write(num1%num2+"<br>");
+document.write(2**3+"<br>");
+// ** -  exponential operator 
+
+// ternary operators
+let a =20;
+let b=10;
+
+(a>b) ? window.alert("a is greater"):window.alert("b is greater");
+
+
