62ce4fac1042961d4b8050c36d122200711e807e
Abhinav <abhinavmule123@gmail.com>
2021-07-19 18:27:00 -0700

    String,object,boolean

---
+++ 19-07-21/script.js
@@ -0,0 +1,113 @@
+// objects
+// let object_name = {property:value,property:value};
+// let student = {
+//     name : "rahul",class : 3,college : "sgbau"
+// };
+
+// console.log(student.class);
+
+//let student = {names : ["rahul","rohan","raj"],class:[11,12,11] };
+// student.names.forEach((value)=>{
+//     console.log(value);
+// });
+
+// let student = {
+//     names : {name:"rohan",class:2},
+//     name1  :{name1 :"raj",class:3}
+// };
+// console.log(student.names.name);
+
+
+// Boolean 
+// let primitiveTrue = true;
+// let primitiveFalse = false;
+// // functions   
+// let functionTrue = Boolean(true);
+// let functionFalse = Boolean(false);
+// // constructor 
+// let constructorTrue  = new Boolean(true);
+// let constructorFalse  = new Boolean(false);
+
+// console.log(primitiveTrue);
+// console.log(primitiveFalse);
+// console.log(functionTrue);
+// console.log(functionFalse);
+// console.log(constructorTrue);
+// console.log(constructorFalse);
+
+
+// string  : - 
+//        01234567891011  
+// let ab = "hello world";
+// let stringLength = ab.length;
+// console.log(stringLength);
+// let ab = "hello";
+
+// let ac = new String("hello");
+// console.log(ab,ac);
+// document.write(ac);
+
+// let str  ='hello';
+// console.log(str);
+
+// String Methods
+//1. concat() :-   
+// let a = "hello";
+// let b = "  world";
+// let c = a.concat(b);
+// console.log(c);
+
+//2.charAt() : -
+// let ab = "hello world";
+// let c = ab.charAt(0);
+// console.log(c);
+
+//3. charCodeAt() : -
+// let ab = "A";
+// let c = ab.charCodeAt(0);
+// console.log(c);
+
+//4. toUpperCase() and 5.toLowerCase():-
+// let ab = "Hello World";
+// let c = ab.toUpperCase();
+// console.log(c);
+
+// let ab = "Hello World";
+// let c = ab.toLowerCase();
+// console.log(c);
+
+//6. trim() : -
+// let ab = "         hello  world        ";
+// let c = ab.trim();
+// console.log(ab);
+// console.log(c);
+
+// //7. replace() :-
+// let ab = "hello world";
+// let c  = ab.replace("hello","hi");
+// console.log(ab);
+// console.log(c);
+
+//8. repeat() :- 
+// let ab = " hello ";
+// let x = ab.repeat(10);
+// console.log(x);
+
+//9.slice(start,end) : -
+// let ab = "hello world";
+// let c = ab.slice(0,5);
+// console.log(c);
+
+//10. indexOf() : -
+// let ab = "hello world";
+// let c =  ab.indexOf("hello");
+// console.log(c);
+
+// let ab = "Hero Honda";
+// if(ab.indexOf("hero") != -1){
+//     console.log("it is present");
+// }else{
+//     console.log("it is not present");
+// }
+
+
