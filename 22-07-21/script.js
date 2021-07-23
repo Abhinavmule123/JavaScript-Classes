a9a3229aceba26470886e1ef99adc35bb857bceb
Abhinav <abhinavmule123@gmail.com>
2021-07-23 11:51:38 -0700

    Date Object

---
+++ 22-07-21/script.js
@@ -0,0 +1,56 @@
+// Number :- 
+// 1. primitive and 2.constructor 
+// let num1 = 23;
+// let num2 = new Number(20);
+// document.write(num1,num2);
+
+// Number methods
+// 4t4i 
+//1. toString() : - binary,octal,hexdecimal,
+//2. toFixed() : - 
+//3. toExponential() : - 
+//4. toPrecision() : - 
+//Number.
+//5. isInteger() : -
+//6. isSafeInteger() : - 2^53 to -2^53
+//7. isFinite() - 
+//8. isNan() : -  //   
+
+// let ab = "30";
+// 1.Number()
+// 2.parseInt()
+// 3.parseFloat()
+
+// isNaN() - valid no -false , not valid no - true
+
+// Infinity and -Infinity
+// floor , round , random,  min , max ,  pow, 
+
+                // Date object
+//miliseconds
+ //                 year,month,day,hours,min,sec,milisecond 
+// let tarikh  = new Date();
+// let todayDate = tarikh.toLocaleDateString();
+// let todayTime = tarikh.toLocaleTimeString();
+
+//set methods
+// tarikh.setFullYear("2000");
+// tarikh.setDate("12");
+// tarikh.setMonth("11");
+// tarikh.setHours("12");
+// tarikh.setMinutes("55");
+// tarikh.setSeconds("59");  
+
+//get methods
+// let day  =tarikh.getDate();
+// let month = tarikh.getMonth();
+// let year = tarikh.getFullYear();
+// let hours = tarikh.getHours();
+// let min = tarikh.getMinutes();
+// let sec = tarikh.getSeconds();
+
+// let todayDate = `Today's Date is :- ${day}-${month+1}-${year}<br>
+// Today's Time :- ${hours}--${min}--${sec}
+// `;
+
+// document.write(todayDate);
