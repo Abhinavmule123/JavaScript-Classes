fa14bee9d2af1a96283d884e4802a0d0ecf34ae5
Abhinav <abhinavmule123@gmail.com>
2021-07-27 18:50:11 -0700

    DOM INTRO

---
+++ 27-07-21/script.js
@@ -0,0 +1,27 @@
+//JavaScript selectors
+// accessing html using id
+// let h1Tag  =  document.getElementById("hello");
+// // window.alert(h1Tag);
+// console.log(h1Tag);
+
+//access html element using class 
+// let h1Tag  =  document.getElementsByClassName("heading");
+// console.log(h1Tag);
+
+//access html element using html tag
+//  let h1Tag =  document.getElementsByTagName("H1");
+//  window.alert(h1Tag);
+// console.log(h1Tag[2]);
+
+// // // 4. querySelector
+// let h1Tag = document.querySelector("#hello #box");
+// console.log(h1Tag);
+
+//5. querySelectorAll():-
+// let h1Tag2 = document.querySelectorAll("h1");
+// for(let i=0;i<h1Tag2.length;i++)
+// {
+//     console.log(h1Tag2[i].id);
+// }
+
+
