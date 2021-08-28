c72dfe232ee970b8868c4b6115c5b220fa4f91c8
Aachal <aachalmahule2017@gmail.com>
2021-08-28 03:35:13 -0700

    todo appp

--- August/12-08-21_P/Task/aachal.js
+++ August/12-08-21_P/Task/aachal.js
@@ -6,7 +6,8 @@ let manageTodo = document.getElementById("manage-todo");
 let manageBtn = document.getElementById("manage-btn");
 let addTodoForm = document.getElementById("add-todo-form");
 let todoList = document.getElementById("todoList");
-
+let manageTabele = document.getElementById("todo-list");
+let completedList = document.getElementById("completedList");
 
 addBtn.onclick = function(){
     bg.style.display = "block";
@@ -21,7 +22,7 @@ addBtn.onclick = function(){
           addTodo(todo.value);
 
       }  else{
-          alert("please enter your todo");
+          toast("please enter your todo");
       }
                                
     }
@@ -40,7 +41,151 @@ function addTodo(todo){
     console.log(li);
 
     todoList.append(li);
-    addTodoForm.requestFullscreen();
+    addTodoForm.reset();
+
+    addToManageTodo(todo);
+    markTodo(markBtn);
+}
+
+function markTodo(markBtn){
+    markBtn.onclick = function(){
+        let li = markBtn.parentElement;
+        let newli = li;
+        li.remove();
+        newli.childNodes[1].className = "unmark-btn";
+        newli.childNodes[1].textContent ="UnMark";
+        console.log(newli);
+        completedList.append(newli);
+        unmarkTodo(newli.childNodes[1]);
+
+    }
+}
+
+function unmarkTodo(unmarkBtn){
+    unmarkBtn.onclick = function(){
+        let li = unmarkBtn.parentElement;
+        let newli = li;
+        li.remove();
+        newli.childNodes[1].className = "mark-btn";
+        newli.childNodes[1].textContent ="Mark";
+        console.log(newli);
+        todoList.append(newli);
+
+        markTodo(newli.childNodes[1]);
+
+    }
+}
+let Srno = 1;
+
+function addToManageTodo(todo){
+    let tr = document.createElement("tr");
+    let sr_td = document.createElement("td");
+    let todo_td = document.createElement("td");
+    let edit_td = document.createElement("td");
+    let del_td = document.createElement("td");
+
+    sr_td.align = "center";
+    todo_td.align = "center";
+    edit_td.align = "center";
+    del_td.align = "center" ;
+
+    sr_td.textContent = Srno++;
+    todo_td.textContent = todo;
+
+    let editBtn = document.createElement("button");
+    editBtn.textContent = "Edit" ;
+    editBtn.className = "edit-btn";
+
+    let delBtn =document.createElement("button");
+    delBtn.textContent = "Delete";
+    delBtn.className = "del_btn";
+
+    edit_td.append(editBtn);
+    del_td.append(delBtn);
+
+    tr.append(sr_td);
+    tr.append(todo_td);
+    tr.append(edit_td);
+    tr.append(del_td);
+
+    manageTabele.append(tr);
+
+    editTodo(editBtn);
+    deleteTodo(delBtn);
+}
+
+
+function editTodo(editBtn){
+    let oldTodoname ;
+    editBtn.onclick = function(){
+        if(editBtn.className =="edit-btn"){
+            editBtn.className = "update-btn";
+            editBtn.textContent = "update";
+            let tr = editBtn.parentElement.parentElement;
+            let todo_td = tr.childNodes[1];
+            oldTodoname = todo_td.textContent;
+            todo_td.setAttribute("contenteditable" ,true);
+            todo_td.style.border = "2px dashed blue";
+            console.log(oldTodoname);
+        }else{
+            editBtn.className = "edit-btn";
+            editBtn.textContent = "edit"
+            let tr = editBtn.parentElement.parentElement;
+            let todo_td = tr.childNodes[1];
+            newTodo = todo_td.textContent;
+            todo_td.setAttribute("contenteditable" ,true);
+            todo_td.style.border = "1px solid black";
+            
+
+            let allLi = document.querySelectorAll("#todoList li");
+            for(i=0; i<allLi.length; i++)
+            if(allLi[i].children[0].textContent.indexOf(oldTodoname) !=-1){
+                allLi[i].children[0].textContent = newTodo;
+                break;
+
+            }
+
+        }
+
+        let compLi = document.querySelectorAll("#completedList li");
+            for(i=0; i<compLi.length; i++)
+            if(compLi[i].children[0].textContent.indexOf(oldTodoname) !=-1){
+                compLi  [i].children[0].textContent = newTodo;
+                break;
+
+            }
+
+        }
+   
+
+    }
+
+
+
+function deleteTodo(delBtn){
+    let oldTodoname
+    delBtn.onclick = function(){
+        alert("hello");
+        let tr = delBtn.parentElement.parentElement; 
+        oldTodoname = tr.childNodes[1].textContent;
+        tr.remove();
+
+        let allLi = document.querySelectorAll("#todoList li");
+        for(i=0; i<allLi.length; i++){
+            if(allLi[i].children[0].innerHTML.indexOf(oldTodoname) != -1){
+                allLi[i].remove();
+            }
+        }
+
+        let completedAllLi = document.querySelectorAll("#todoList li");
+        for(i=0; i<completedAllLi.length; i++){
+            if(completedAllLi[i].children[0].innerHTML.indexOf(oldTodoname) != -1){
+                completedAllLi[i].remove();
+            }
+        }
+
+    }
+
 
 }
 
@@ -54,4 +199,15 @@ bg.onclick = function(){
 manageBtn.onclick = function(){
     bg.style.display = "block";
     manageTodo.style.left = "50%";
+}
+function toast(msg){
+let toast = document.getElementById("toast");
+let toastText = document.getElementById("toast-text");
+toastText.textContent = msg;
+toast.style.display = "block";
+
+setTimeout(function(){
+    toast.style.display = "none";
+},2000);
+
 }
