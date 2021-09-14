203a081dd2f9144ec3be2209021d7f827ab50ae0
Abhinav <abhinavmule123@gmail.com>
2021-09-13 17:09:52 -0700

    animate,stop,css,text,val,html methods

---
+++ September/13-09-21/script.js
@@ -0,0 +1,62 @@
+$(document).ready(function(){
+    // $("div").animate({
+    //     left: "1200px"
+    // },5000);
+    // $("div").animate({
+    //     top: "450px"
+    // },2000);
+    // $("div").animate({
+    //     left: "30px"
+    // },2000);
+    // $("div").animate({
+    //     top: "30px"
+    // },2000);
+    // $("h1").animate({
+    //     fontSize: "50px"
+    // },2000);
+
+    // $("#stop-btn").click(funct'ion(){
+    //     $("div").stop();
+    // })
+// const h1 = document.getElementById("heading");
+// h1.style.color = "red";
+// h1.style.fontFamily = "cursive";
+
+    // $("h1").css("color","red");
+    // $("h1").css({
+    //     color : "red",
+    //     fontSize : "30px",
+    //     fontFamily : "sans-serif"
+    // });
+
+    // const h1 = document.getElementById("heading");
+    // h1.className = "heading";
+
+    //  const str = h1.classList;
+    // str.add("add","helo");
+
+    // $("h1").addClass("heading");
+    // $("h1").removeClass("hello");
+    // $("#toggle-class").click(function(){
+    //     $("h1").toggleClass("heading");
+    // });
+
+    // text()    = innerText,textContent 
+    // html()    = innerHTML
+    // val()     = .value
+
+
+    // console.log($("#heading").text());
+    // console.log($("div").html());
+
+
+    // $("form").submit(function(e){
+    //     e.preventDefault();
+    //     console.log($("#name").val());
+    // });
+
+
+    //$("#heading").text("Hello india");
+    //$("div").html("<h1>This is world</h1>");
+   // $("#name").val("Hello world");
+});
