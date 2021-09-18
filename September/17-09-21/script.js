f44d25b46bbcd6fe10682e34d0c35d3249652e45
Abhinav <abhinavmule123@gmail.com>
2021-09-17 20:00:51 -0700

    jQuery Traversing

---
+++ September/17-09-21/script.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+  //  $("#container").slideUp(2000).slideDown(2000).fadeOut(2000);
+
+    // $("h1").parent().css({
+    //     border : "2px solid red"
+    // })
+
+
+    //$("#second").parents().css("border","2px solid red");
+
+  //  $("#second").parentsUntil("body").css("border","2px solid red");
+
+  //  $("ul").children("#second").css("border","2px solid red");
+
+   // $("ul").find("#second").css("border","2px solid red");
+
+
+ //  $("h3").siblings().css("color","red");
+
+    // $("h3").prev().css("color","red");
+
+   // $("h3").prevAll().css("color","red");
+
+  // $("h4").prevUntil("h1").css("color","red");
+
+
+//   $("h3").next().css("color","red");
+    // $("h3").nextAll().css("color","red");
+ //   $("h2").nextUntil("h6").css("color","red");
+
+    // jQuery filtering  
+    // $("td").first().css("backgroundColor","yellow");
+
+   // $("tr").last().css("backgroundColor","yellow");
+
+   //   $("th").eq(1).css("backgroundColor","yellow");
+
+
+   //$("tr").filter("#head").css("backgroundColor","yellow");
+
+   // $("tr").not("#head").css("backgroundColor","yellow");
+
+   
+});
